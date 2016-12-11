f91fb7739d1ac67fe9b755654069697c6fdc21b9
Michał Sieński <michal.sienski1987@gmail.com>
2016-12-11 15:38:30 +0100

    add some jquery code - not working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var submit = document.querySelector('.submit-button');
-    var formInProgress = document.querySelector('.in-progress');
-    var formAccepted = document.querySelector('.form-accepted');
+$(document).ready(function () {
 
-    submit.addEventListener('click', function (event) {
-        event.preventDefault();
-        formInProgress.style.display = 'none';
-        formAccepted.style.display = 'block';
-    });
-})
+    var $submit = $('.submit-button');
+    var $formInProgress = $('.in-progress');
+    var $formAccepted = $('.form-accepted');
+
+    var $name = $("#name");
+    var $email = $("#email");
+    var $country = $('#country');
+    var $errorOne = $(".error-one");
+    var $errorTwo = $(".error-two");
+
+    function validation() {
+        $submit.on("click", function (e) {
+            var nameValue = $name.val();
+            var emailValue = $email.val();
+            e.preventDefault();
+
+            if (nameValue.length < 5) {
+                $errorOne.text("To pole musi mieć co najmniej 5 znakow");
+                $errorOne.show();
+            } else {
+                $errorOne.text("");
+                $errorOne.hide();
+            }
+
+            if (emailValue.length < 5 || emailValue.search("@") === -1) {
+                $errorTwo.text("Email musi mieć co najmniej 5 znaków i zawierać znak @");
+                $errorTwo.show();
+            } else {
+                $errorTwo.text("");
+                $errorTwo.hide();
+            }
+
+        });
+    }
+
+    validation();
+
+});
